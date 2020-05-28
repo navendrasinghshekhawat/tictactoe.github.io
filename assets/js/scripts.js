e0392707d2dc925c424ccf5d9208f5b0540fa5c7
navendrasinghshekhawat <58545722+navendrasinghshekhawat@users.noreply.github.com>
2020-05-28 16:07:16 +0530

    test
     test

---
+++ assets/js/scripts.js
@@ -0,0 +1,55 @@
+var a11=document.getElementById('one');
+var a12=document.getElementById('two');
+var a13=document.getElementById('three');
+
+var a21=document.getElementById('four');
+var a22=document.getElementById('five');
+var a23=document.getElementById('six');
+
+var a31=document.getElementById('seven');
+var a32=document.getElementById('eight');
+var a33=document.getElementById('nine');
+
+//function set(a11){
+//	a11.innerHtml="<strong>X</strong>"
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function test()
+// {
+// console.log(a11);
+// console.log(a12);
+// console.log(a13);
+
+// console.log(a21);
+// console.log(a22);
+// console.log(a23);
+
+// console.log(a31);
+// console.log(a32);
+// console.log(a33);
+// };
+
