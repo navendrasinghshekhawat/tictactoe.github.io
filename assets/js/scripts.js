e9b92c1419dfc4915b2cfbff68f3edaa10998528
navendrasinghshekhawat <58545722+navendrasinghshekhawat@users.noreply.github.com>
2020-05-29 19:22:53 +0530

    flex

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,72 +16,93 @@ reset.addEventListener('click',function(){
 location.reload();
 });
 
-//function set(a11){
-//	a11.innerHtml="<strong>X</strong>"
-//}
-//function pass(){
-
+//////
 	//on single click
+var i=0;
+
 a11.addEventListener('click',function(){
+	if(i%2==0){
 a11.innerText="X";
-console.log("hello");
+console.log(a11);
+i=i+1;}
+else {
+	a11.innerText="O";
+i=i+1;
+}
 });
 a12.addEventListener('click',function(){
+	if(i%2==0){
 a12.innerText="X";
+i=i+1;}
+else {
+	a12.innerText="O";
+i=i+1;
+}
 });
 a13.addEventListener('click',function(){
+	if(i%2==0){
 a13.innerText="X";
+i=i+1;}
+else {
+	a13.innerText="O";
+i=i+1;
+}
 });
 a21.addEventListener('click',function(){
+	if(i%2==0){
 a21.innerText="X";
+i=i+1;}
+else {
+	a21.innerText="O";
+i=i+1;
+}
 });
 a22.addEventListener('click',function(){
+	if(i%2==0){
 a22.innerText="X";
+i=i+1;}
+else {
+	a22.innerText="O";
+i=i+1;
+}
 });
 a23.addEventListener('click',function(){
+	if(i%2==0){
 a23.innerText="X";
+i=i+1;}
+else {
+	a23.innerText="O";
+i=i+1;
+}
 });
 a31.addEventListener('click',function(){
+	if(i%2==0){
 a31.innerText="X";
+i=i+1;}
+else {
+	a31.innerText="O";
+i=i+1;
+}
 });
 a32.addEventListener('click',function(){
+	if(i%2==0){
 a32.innerText="X";
+i=i+1;}
+else {
+	a32.innerText="O";
+i=i+1;
+}
 });
 a33.addEventListener('click',function(){
+	if(i%2==0){
 a33.innerText="X";
+i=i+1;}
+else {
+	a33.innerText="O";
+i=i+1;
+}
 });
 
-//on double click
-a11.addEventListener('dblclick',function(){
-a11.innerText="O";
-});
-a12.addEventListener('dblclick',function(){
-a12.innerText="O";
-});
-a13.addEventListener('dblclick',function(){
-a13.innerText="O";
-});
-a21.addEventListener('dblclick',function(){
-a21.innerText="O";
-});
-a22.addEventListener('dblclick',function(){
-a22.innerText="O";
-});
-a23.addEventListener('dblclick',function(){
-a23.innerText="O";
-});
-a31.addEventListener('dblclick',function(){
-a31.innerText="O";
-});
-a32.addEventListener('dblclick',function(){
-a32.innerText="O";
-});
-a33.addEventListener('dblclick',function(){
-a33.innerText="O";
-});
-//}
-
-//pass();
 
 
 
@@ -89,33 +110,4 @@ a33.innerText="O";
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function test()
-// {
-// console.log(a11);
-// console.log(a12);
-// console.log(a13);
-
-// console.log(a21);
-// console.log(a22);
-// console.log(a23);
-
-// console.log(a31);
-// console.log(a32);
-// console.log(a33);
-// };
 
