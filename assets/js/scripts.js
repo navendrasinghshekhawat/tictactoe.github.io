2ad01cf4985e05b59e44b47fb72a1a3c8c07001a
navendrasinghshekhawat <58545722+navendrasinghshekhawat@users.noreply.github.com>
2020-05-29 17:34:58 +0530

    s
    s

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,9 +13,69 @@ var a33=document.getElementById('nine');
 //function set(a11){
 //	a11.innerHtml="<strong>X</strong>"
 //}
+//function pass(){
 
+	//on single click
+a11.addEventListener('click',function(){
+a11.innerText="X";
+console.log("hello");
+});
+a12.addEventListener('click',function(){
+a12.innerText="X";
+});
+a13.addEventListener('click',function(){
+a13.innerText="X";
+});
+a21.addEventListener('click',function(){
+a21.innerText="X";
+});
+a22.addEventListener('click',function(){
+a22.innerText="X";
+});
+a23.addEventListener('click',function(){
+a23.innerText="X";
+});
+a31.addEventListener('click',function(){
+a31.innerText="X";
+});
+a32.addEventListener('click',function(){
+a32.innerText="X";
+});
+a33.addEventListener('click',function(){
+a33.innerText="X";
+});
 
+//on double click
+a11.addEventListener('dblclick',function(){
+a11.innerText="O";
+});
+a12.addEventListener('dblclick',function(){
+a12.innerText="O";
+});
+a13.addEventListener('dblclick',function(){
+a13.innerText="O";
+});
+a21.addEventListener('dblclick',function(){
+a21.innerText="O";
+});
+a22.addEventListener('dblclick',function(){
+a22.innerText="O";
+});
+a23.addEventListener('dblclick',function(){
+a23.innerText="O";
+});
+a31.addEventListener('dblclick',function(){
+a31.innerText="O";
+});
+a32.addEventListener('dblclick',function(){
+a32.innerText="O";
+});
+a33.addEventListener('dblclick',function(){
+a33.innerText="O";
+});
+//}
 
+//pass();
 
 
 
